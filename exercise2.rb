704b131e9679e8bc60015e9c361a8fc5690d54ef
Rich Seviora <richard.seviora@gmail.com>
2014-10-21 10:55:22 -0400

    Exercise 2 Complete

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# Calculating tip on meal.
+puts 55 * 0.20
+
+#Adding string and integer
+puts 55 + "55".to_f
+
+#Big math using string interpolation.
+puts "#{45628*7839}"
+
+#Value of statement expected to be true as statement are ORs, and last statement !(false && false) is expected to be false.
+puts (true && false) || (false && true) || !(false && false)
